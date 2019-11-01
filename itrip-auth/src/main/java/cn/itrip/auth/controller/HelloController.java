24deb9398b62b016ed3e72f972b5e141888ed446
test <test@test.com>
2019-11-01 17:32:51 +0800

    v5.3 auth添加hellocontroller

---
+++ itrip-auth/src/main/java/cn/itrip/auth/controller/HelloController.java
@@ -0,0 +1,13 @@
+package cn.itrip.auth.controller;
+
+/**
+ * description:
+ * Created by Ray on 2019-11-01
+ */
+public class HelloController {
+    public void test11(){
+        System.out.println("hello123");
+        System.out.println("hello123");
+        System.out.println("hello123");
+    }
+}
